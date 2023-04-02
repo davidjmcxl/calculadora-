8184d1c37a8c53d7cb64cf420248c7b59d55cb47
David Moreno <davidjmcxl@gmail.com>
2023-04-02 18:07:56 -0500

    1 version calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadoraAp';
+  operation:string='0';
+  evaluar:string='0';
+  result: string = '';
+
+  appendNumber(number: number) {
+    if(this.operation=='0'){
+      this.operation='';
+    }
+    this.operation += number.toString();
+  }
+  deleteLast(){
+    this.operation=this.operation.slice(0,-1);
+  }
+
+  appendOperator(operator: string) {
+      if(operator=='*'){
+        operator='x';
+      }
+    this.operation += '' + operator + '';
+  }
+
+  clear() {
+    this.operation = '0';
+    this.result = '';
+  }
+
+  calculate() {
+    try {
+
+      if(this.operation.includes('x')){
+        let operationArray=this.operation.split("");
+       let transf= operationArray.map(element=>{
+          if(element=='x'){
+            element='*';
+          }
+          return element
+        });
+        this.evaluar=transf.join("");
+        this.result =eval(this.evaluar);
+      }
+      else
+      {
+        this.result = eval(this.operation);
+      }
+
+
+
+
+    } catch (e) {
+      this.result = 'Error';
+    }
+  }
+
 }
